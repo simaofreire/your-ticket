3c06926a102352ed4286ee74dc56ec6f58de3b32
Simão Freire <simaofreire@live.com>
2023-08-10 00:58:45 -0300

    feat(app): Added error middleware

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application } from 'express';
 import { connect } from './infra/database';
+import { errorMiddleware } from './middlewares/error.middleware';
 
 class App {
 	public app: Application;
@@ -17,7 +18,7 @@ class App {
 	}
 
 	intercepErrors() {
-		// this.app.use()
+		this.app.use(errorMiddleware);
 	}
 
 	initializeMiddlewares() {
